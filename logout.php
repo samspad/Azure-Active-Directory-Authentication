8a04e2df9cee6b30849ae3d2d3bc26eb9c2c2762
Ali <shahidali@marshall.edu>
2022-02-07 10:16:00 +0530

    additional logout code added

--- logout.php
+++ logout.php
@@ -3,6 +3,23 @@
         session_start();
     }
 
+
+    // ######## store loging logs logout time  #######
+            /*  $date = date('Y-m-d H:i:s');
+                $logid = $_SESSION['login_log_id'];
+                $sect_ql ="		
+                UPDATE [dbo].[login_logs] SET 			
+                    [logout_time] = '$date'
+                WHERE [login_log_id] = '$logid'   ";
+                //echo  $sect_ql;
+
+                $stmt = sqlsrv_query( $conn1, $sect_ql );
+                if( $stmt === false) {
+                    die( print_r( sqlsrv_errors(), true) );
+                }*/
+
+    /* ############ Logs End ###########3 */
+
     unset($_SESSION['user_id']);
     session_destroy();
     $return_url = "";
